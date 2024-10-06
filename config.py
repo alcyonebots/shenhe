92d3c0f69a70eb590066fa0cb27551b8a05ff992
nxivm-bots <massacres@myyahoo.com>
2024-10-07 02:52:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7465723607:AAGw0JGtHxQrTTU9u4ivlSjgshr4I-ZvDcw"
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://gay:gay@cluster0.wthde.mongodb.net/")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180))
 
 # Chat id of a group for logging bot's activities
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002183841044"))
