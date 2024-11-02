23b3e5abd152ec05e9987f50ac2dedb34aecfdd5
nxivm-bots <massacres@myyahoo.com>
2024-11-02 21:25:34 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -1,17 +1,20 @@
 import os
 import logging
 from PIL import Image
+
 async def gen_thumb(videoid: str):
     try:
         if os.path.isfile(f"cache/{videoid}_v4.png"):
             return f"cache/{videoid}_v4.png"
-        default_thumbnail_path = "AviaxMusic/assets/shenhe.png"  # Change this to your default image path
-        # Open the default thumbnail
+
+        default_thumbnail_path = "AviaxMusic/assets/shenhe.png"
         youtube = Image.open(default_thumbnail_path)
-        # Save the thumbnail directly without any modifications
+
         background_path = f"cache/{videoid}_v4.png"
         youtube.save(background_path)
+        
         return background_path
+
     except Exception as e:
         logging.exception(f"Error generating thumbnail for video {videoid}: {e}")
         return None
