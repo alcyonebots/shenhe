a0c3870be4c3f4d36748b80d63695d407dea81b6
nxivm-bots <massacres@myyahoo.com>
2024-11-02 22:14:47 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -1,21 +1,25 @@
 import os
 import logging
+import random
 from PIL import Image
+
+thumbnail_paths = [
+    "AviaxMusic/assets/shenhe.png", 
+    "AviaxMusic/assets/shenhe1.png" 
+]
 
 async def gen_thumb(videoid: str):
     try:
         if os.path.isfile(f"cache/{videoid}_v4.png"):
             return f"cache/{videoid}_v4.png"
-
-        default_thumbnail_path = "AviaxMusic/assets/shenhe.png"
         
-        youtube = Image.open(default_thumbnail_path)
-
+        selected_thumbnail_path = random.choice(thumbnail_paths)
+        
+        youtube = Image.open(selected_thumbnail_path)
+        
         background_path = f"cache/{videoid}_v4.png"
         youtube.save(background_path)
-        
         return background_path
-
     except Exception as e:
         logging.exception(f"Error generating thumbnail for video {videoid}: {e}")
         return None
